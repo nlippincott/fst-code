28017fe974be712216b478a6e95b9b7db2d362fd
nlippincott <nl@acm.org>
2026-03-20 02:54:33 -0400

    Added FST controller for serving files

---
+++ php/code_controller.php
@@ -0,0 +1,19 @@
+<?php
+
+class code_controller extends FST\Controller {
+
+	public function init () {
+
+		// Determine file name
+		$fname = __DIR__ . '../' . $this->args();
+
+		// Check if file exists
+		if (!file_exists($fname))
+			$this->notfound();
+
+		// Send the file
+		FST\Framework::serve($fname);
+
+		exit;
+	}
+}
